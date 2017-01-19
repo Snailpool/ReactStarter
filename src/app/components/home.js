1dd125b0b555ea1a4ad9a88b25b8a0bb36d21915
你看看你 <Snail@nikankannide-MacBook-Pro.local>
2017-01-19 19:36:09 +0800

    Create this starter

---
+++ src/app/components/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import CSSModules from 'react-css-modules';
+import styles from './home.css';
+
+class Home extends React.Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (
+			<div >
+				<h1>apple</h1>
+			</div>
+		);
+	}
+}
+
+export default Home;
+
